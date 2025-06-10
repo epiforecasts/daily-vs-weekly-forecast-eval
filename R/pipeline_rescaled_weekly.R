3ed1238190ff192eef5e4ff6669522cdd48c8b12
James Azam <james.m.azam@gmail.com>
2025-06-10 18:49:21 +0100

    Move code out shared code and restructure pipeline scripts for readability

--- R/pipeline_rescaled_weekly.R
+++ R/pipeline_rescaled_weekly.R
@@ -3,8 +3,6 @@ library(data.table)
 library(parallel)
 library(bayesplot)
 
-options(mc.cores = parallel::detectCores() - 1)
-
 .args <- if (interactive()) {
     .prov <- "GP"
     .tmp <- sprintf(
@@ -23,27 +21,10 @@ options(mc.cores = parallel::detectCores() - 1)
 # Load helper functions and shared model inputs
 source(.args[length(.args) - 1])
 
-# inflate as.Date, because EpiNow2 seems to prefer Date over IDate
-dt <- readRDS(.args[1])[, .(date = as.Date(date), confirm)][!is.na(confirm)]
 
-# EpiNow wants to work in terms of days, so we're going to pretend
-# as if weeks are days
-dt[, orig_date := date]
-
-fake_daily_dates <- seq.Date(
-  from = dt$orig_date[1],
-  by = "day",
-  length.out = length(dt$orig_date)
-)
-
-dt$date <- fake_daily_dates
-
-# Train and forecast windows
-train_window <- 10 # 10 weeks
-test_window <- 2 # 2 weeks
-
-slides <- seq(0, dt[, .N - (train_window + test_window)], by = test_window)
-
+####################################
+# Parameters
+####################################
 # when changing units, the mean, sd, and max scale the same way
 incubation_period <- LogNormal(mean = 5 / 7, sd = 1 / 7, max = 14 / 7)
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7201952/
@@ -62,7 +43,9 @@ delay <- incubation_period + reporting_delay
 # Rt prior
 rt_prior <- LogNormal(meanlog = 0.69, sdlog = 0.05) # mean = 2, sd = 0.1
 
+####################################
 # Observation model
+####################################
 obs <- obs_opts(
   week_effect = FALSE,
   likelihood = TRUE,
@@ -72,13 +55,34 @@ obs <- obs_opts(
 ###############################
 # Pipeline
 ###############################
+
+# Prepare data
+# inflate as.Date, because EpiNow2 seems to prefer Date over IDate
+dt <- readRDS(.args[1])[, .(date = as.Date(date), confirm)][!is.na(confirm)]
+
+# EpiNow wants to work in terms of days, so we're going to pretend
+# as if weeks are days
+dt[, orig_date := date]
+
+fake_daily_dates <- seq.Date(
+  from = dt$orig_date[1],
+  by = "day",
+  length.out = length(dt$orig_date)
+)
+
+dt$date <- fake_daily_dates
+
+# Slides for fitting
+slides <- seq(0, dt[, .N - (train_window_rescaled + test_window_rescaled)], by = test_window_rescaled)
+
+# Fit models
 res_dt <- lapply(slides, \(slide) {
-    slice <- dt[seq_len(train_window) + slide] |> trim_leading_zero()
+    slice <- dt[seq_len(train_window_rescaled) + slide] |> trim_leading_zero()
     # Slides for fitting are in weeks but we need to rescale back to
     # days for aligning with other scales
     slide_rescaled <- slide * 7
     # Fit model
-    if (slice[, .N > test_window * 2]) {
+    if (slice[, .N > test_window_rescaled * 2]) {
         # diagnostics place holder to guarantee entry into while
         diagnostics <- data.table(
             divergent_transitions = 20,
@@ -107,7 +111,7 @@ res_dt <- lapply(slides, \(slide) {
                 generation_time = generation_time_opts(generation_time),
                 delays = delay_opts(delay),
                 rt = rt_opts(prior = rt_prior),
-                forecast = forecast_opts(horizon = test_window),
+                forecast = forecast_opts(horizon = test_window_rescaled),
                 obs = obs,
                 stan = next_stan
             )
@@ -149,7 +153,7 @@ res_dt <- lapply(slides, \(slide) {
         res_dt
     } else {
         empty_forecast <- data.table(
-            date = dt[train_window + slide, date + seq_len(test_window)],
+            date = dt[train_window_rescaled + slide, date + seq_len(test_window_rescaled)],
             sample = NA_integer_, value = NA_integer_, slide = slide_rescaled
         )
         data.table(
